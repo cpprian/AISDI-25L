57cc4235dbd6556f521424b26186cacc9a1d6709
cpprian <cpprian456@gmail.com>
2025-04-04 22:21:48 +0200

    add benchmarks

--- heap/heap.py
+++ heap/heap.py
@@ -1,4 +1,4 @@
-class Heap:
+class DHeap:
     def __init__(self, _d):
         self.d = _d
         self.heap = []
@@ -7,8 +7,13 @@ class Heap:
         return (i - 1) // self.d
 
     def children(self, i):
-        return [self.d * i + j + 1 for j in range(self.d) if self.d * i + j + 1 < len(self.heap)]
-    
+        children = []
+        for j in range(self.d):
+            child_index = self.d * i + j + 1
+            if child_index < len(self.heap):
+                children.append(child_index)
+        return children
+
     def append(self, value):
         self.heap.append(value)
         self.heapify_up(len(self.heap) - 1)
@@ -16,18 +21,21 @@ class Heap:
     def pop(self):
         if not self.heap:
             return None
-        
+
         top = self.heap[0]
         last = self.heap.pop()
         if self.heap:
             self.heap[0] = last
-            self.heapify_down
+            self.heapify_down(0)
         return top
 
     def heapify_up(self, i):
         while i > 0 and self.heap[i] < self.heap[self.parent(i)]:
-            self.heap[i], self.heap[self.parent(i)] = self.heap[self.parent(i)], self.heap[i]
-            i = i - 1
+            self.heap[i], self.heap[self.parent(i)] = (
+                self.heap[self.parent(i)],
+                self.heap[i],
+            )
+            i = self.parent(i)
 
     def heapify_down(self, i):
         while True:
@@ -36,7 +44,7 @@ class Heap:
             if not children:
                 break
 
-            min_child = min(children, key=lambda x: self.heap(x))
+            min_child = min(children, key=lambda x: self.heap[x])
             if self.heap[i] > self.heap[min_child]:
                 self.heap[i], self.heap[min_child] = self.heap[min_child], self.heap[i]
                 i = min_child
---
+++ heap/testing.py
@@ -0,0 +1,52 @@
+import pytest
+import random
+from heap import DHeap
+
+
+@pytest.fixture(scope="module")
+def input_data():
+    return [random.randint(1, 300000) for _ in range(100000)]
+
+
+@pytest.mark.parametrize("d", [2, 5, 7])
+def test_heap_correctness(d):
+    heap = DHeap(d)
+    input_data = [random.randint(1, 300000) for _ in range(10000)]
+
+    for v in input_data:
+        heap.append(v)
+
+    sorted_data = sorted(input_data)
+    for i in range(len(sorted_data)):
+        assert heap.pop() == sorted_data[i]
+
+
+@pytest.mark.benchmark(group="append")
+@pytest.mark.parametrize("d", [2, 5, 7])
+def test_benchmark_append(d, benchmark, input_data):
+    def setup():
+        heap = DHeap(d)
+        return (heap,), {}
+
+    def append_all(h: DHeap):
+        for v in input_data:
+            h.append(v)
+
+    benchmark.pedantic(append_all, setup=setup, rounds=10)
+
+
+@pytest.mark.benchmark(group="pop")
+@pytest.mark.parametrize("d", [2, 5, 7])
+def test_benchmark_pop(d, benchmark, input_data):
+    def setup():
+        heap = DHeap(d)
+        for v in input_data:
+            heap.append(v)
+
+        return (heap,), {}
+
+    def remove_all(h: DHeap):
+        for _ in range(10000):
+            h.pop()
+
+    benchmark.pedantic(remove_all, setup=setup, rounds=10)
